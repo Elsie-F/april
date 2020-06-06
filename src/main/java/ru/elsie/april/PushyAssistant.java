706c5c944209319319ef4442605840f8fa08cbb9
Elsie-F <e-filipp@yandex.ru>
2020-06-06 15:34:56 +0300

    it's possible to choose impl

---
+++ src/main/java/ru/elsie/april/PushyAssistant.java
@@ -0,0 +1,8 @@
+package ru.elsie.april;
+
+public class PushyAssistant implements Assistant {
+    @Override
+    public void assist() {
+        System.out.println("I will help you anyway");
+    }
+}
--- src/main/java/ru/elsie/april/infrastructure/ObjectFactory.java
+++ src/main/java/ru/elsie/april/infrastructure/ObjectFactory.java
@@ -1,16 +1,22 @@
 package ru.elsie.april.infrastructure;
 
 import lombok.SneakyThrows;
+import ru.elsie.april.Assistant;
+import ru.elsie.april.PushyAssistant;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ObjectFactory {
     private static ObjectFactory ourInstance = new ObjectFactory();
-    private Config config = new JavaConfig("ru.elsie.april");
+    private Config config;
 
     public static ObjectFactory getInstance() {
         return ourInstance;
     }
 
     private ObjectFactory() {
+         config = new JavaConfig("ru.elsie.april", new HashMap<>(Map.of(Assistant.class, PushyAssistant.class)));
     }
 
     @SneakyThrows
